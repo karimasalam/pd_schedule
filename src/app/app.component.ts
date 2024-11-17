4e537e13e92bc7f89b2f531a1e234af6513d4c90
Karim Abdelsalam <karim.ahmed@outlook.com>
2024-11-17 14:34:37 +1100

    refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ScheduleService } from './schedule.service';
 import * as XLSX from 'xlsx';
-import { RenderedScheduleEntry } from '../interfaces/schedule';
+import { RenderedScheduleEntry } from './interfaces/schedule';
 import { getWeekNumber } from './utilities';
 
 
@@ -15,7 +15,7 @@ export class AppComponent {
   fromDate: string = '2024-07-01T10:00';
   toDate: string = '2024-07-30T10:00';
   id: string = '';
-  schedules?: RenderedScheduleEntry [];
+  schedules?: RenderedScheduleEntry[];
   isLoading: boolean = false;
   payment: string = "35.71";
 
@@ -28,7 +28,6 @@ export class AppComponent {
         next: (data) => {
           this.schedules = data.schedule.final_schedule.rendered_schedule_entries;
           this.isLoading = false;
-          console.log(data);
           console.log(this.schedules);
         },
         error: (err) => {
@@ -39,34 +38,31 @@ export class AppComponent {
   }
 
   exportToExcel(): void {
-    if(this.schedules){
+    if (this.schedules) {
 
-    var arr: any[] = [];
-    this.schedules.forEach(schedule => {
-      var startdate = new Date(schedule.start);
-      console.log(startdate);
-      var enddate = new Date(schedule.end);
+      var arr: any[] = [];
+      this.schedules.forEach(schedule => {
+        var startdate = new Date(schedule.start);
+        var enddate = new Date(schedule.end);
 
-      while(enddate > startdate)
-      {
+        while (enddate > startdate) {
           arr.push({
             Date: startdate.toDateString(),
-            Name: schedule.user.summary, 
+            Name: schedule.user.summary,
             Week: getWeekNumber(startdate),
             Payment: this.payment,
             Month: startdate.toLocaleString('default', { month: 'long' })
           });
           startdate = new Date(startdate.setDate(startdate.getDate() + 1));
 
-      }
-    });
-    console.log(arr);
-      const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(arr  );
+        }
+      });
+      const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(arr);
       const wb: XLSX.WorkBook = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(wb, ws, 'Schedules');
 
       XLSX.writeFile(wb, `PD_schedules.xlsx`);
     }
-    
+
   }
 }
